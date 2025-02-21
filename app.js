54484762a5df65fff40b6812722f1b436e185cdd
Sanskriti singh <146508510+Sanskriti3104@users.noreply.github.com>
2025-02-21 09:56:59 +0530

    Add styling and Classic and Rainbow button

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const userInput = document.getElementById("quantity");
 const reset = document.querySelector(".reset");
 const grid = document.querySelector("#gridContainer");
+const rainbow = document.querySelector(".rainbow");
+const classic = document.querySelector(".classic");
+let mode = "classic";
 
 createGrid = () => {
     grid.innerHTML = "";
     const gridSize = 16;
     const squareSize = 575 / gridSize;
 
-    for(let i=0; i<gridSize * gridSize; i++){
+    for (let i = 0; i < gridSize * gridSize; i++) {
         const sq = document.createElement("div");
         sq.classList.add("square");
         sq.style.height = `${squareSize}px`;
         sq.style.width = `${squareSize}px`;
         grid.appendChild(sq);
     }
+
+    addSquareEventListeners();
 };
 
 updateGrid = () => {
@@ -21,23 +26,48 @@ updateGrid = () => {
     const gridSize = userInput.value;
     const squareSize = 575 / gridSize;
 
-    for(let i=0; i<gridSize * gridSize; i++){
+    for (let i = 0; i < gridSize * gridSize; i++) {
         const div = document.createElement("div");
         div.classList.add("square");
         div.style.height = `${squareSize}px`;
         div.style.width = `${squareSize}px`;
         grid.appendChild(div);
     }
+
+    addSquareEventListeners();
 };
 
-userInput.addEventListener("change",updateGrid);
+userInput.addEventListener("change", updateGrid);
 
-const square = document.querySelector("div");
-square.addEventListener("mouseover", function(event) {
-    event.target.classList.replace("square","color");
+const addSquareEventListeners = () => {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        if (mode === "classic") {
+            square.addEventListener("mouseover", function (event) {
+                event.target.style.backgroundColor = "grey";
+                event.target.classList.replace("square", "color");
+            });
+        } else {
+            square.addEventListener("mouseover", function (event) {
+                const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+                event.target.style.backgroundColor = "#" + randomColor;
+                event.target.classList.replace("square", "color");
+            });
+        }
+    });
+};
+
+classic.addEventListener("click", function () {
+    mode = "classic";
+    addSquareEventListeners();
 });
 
-reset.addEventListener("click",function() {
+rainbow.addEventListener("click", function () {
+    mode = "rainbow";
+    addSquareEventListeners();
+})
+
+reset.addEventListener("click", function () {
     grid.innerHTML = "";
     userInput.value = "";
     createGrid();
